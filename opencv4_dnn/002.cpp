39a0635dec497015500b610b6d5be7923c7a9d5a
zhixun <327354573@qq.com>
2021-06-11 20:38:27 +0800

    20210611

--- opencv4_dnn/002.cpp
+++ opencv4_dnn/002.cpp
@@ -28,18 +28,18 @@ int main(int argc, char** argv) {
 		auto layer = net.getLayer(id);
 		//printf("layer id : %d, type : %s, name : %s \n", id, layer->type.c_str(), layer->name.c_str());
 	}
-	Mat src = imread("./home.jpg");
+	Mat src = imread("./xiaomaolu.jpg");
 	imshow("input", src);
 	vector<string> names = readLabels();
 
 	// 构建输入 模型配置 https://github.com/opencv/opencv/blob/master/samples/dnn/models.yml
 	// 函数介绍https://www.zhihu.com/column/2know 
-	int w = 224;
-	int h = 224;
-	Mat intpuBlob = blobFromImage(src, 1.0, Size(w, h), Scalar(104, 117, 123), false, false);
+	int w = 300;
+	int h = 300;
+	Mat intpuBlob = blobFromImage(src, 0.007843, Size(w, h), Scalar(127.5, 127.5, 127.5), false, false);
 
 	// 设置输入
-	net.setInput(intpuBlob);
+	net.setInput(intpuBlob, "data");
 	// 推断  函数返回一个Mat变量，返回值是指输入的layername首次出现的输出。
 	//例如  输入 prob 就能返回该层的结果, 没有参数返回最后一层输出结果
 	/*layer{
@@ -53,19 +53,19 @@ int main(int argc, char** argv) {
 	Mat probMat = net.forward();
 	cout << "channel = " << probMat.channels() << "\trows = " << probMat.rows << "\tcols = " << probMat.cols << endl;
 
-	//解析数据
-	Mat prob = probMat.reshape(1, 1);//变成一维数组
-
-	Point classNum;
+	//SSD 模型结果都存储在结构体size 中  
+	Mat detectionOut(probMat.size[2], probMat.size[3], CV_32F, probMat.ptr<float>());
+	cout << "detectionOut-channel = " << detectionOut.channels() << "\trows = " << detectionOut.rows << "\tcols = " << detectionOut.cols << endl;
+	/*Point classNum;
 	double classProb;
 	minMaxLoc(prob, NULL,//最小值指针
-		&classProb,//最大值指针 
+		&classProb,//最大值指针
 		NULL,//最小值位置指针
 		&classNum);//最大值位置指针
 	int index = classNum.x;
 	printf("\n current index = %d, possible : %.2f , name : %s\n", index, classProb, names[index].c_str());
 	putText(src, names[index].c_str(), Point(50, 50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 255), 2, 8);
-	imshow("result", src);
+	imshow("result", src);*/
 	waitKey(0);
 	return 0;
 }
